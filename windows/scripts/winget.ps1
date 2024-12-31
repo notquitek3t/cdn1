2b17aa48c3eba57f3af0e85ac44f008a572f4705
kaii :3 (notquitek3t) <72680058+notquitek3t@users.noreply.github.com>
2024-12-31 13:52:45 -0700

    winget fixes

--- windows/scripts/winget.ps1
+++ windows/scripts/winget.ps1
@@ -1,17 +1,17 @@
-Invoke-WebRequest `
-    -URI https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3 `
-    -OutFile xaml.zip -UseBasicParsing
-New-Item -ItemType Directory -Path xaml
-Expand-Archive -Path xaml.zip -DestinationPath xaml
-Add-AppxPackage -Path "xaml\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
-Remove-Item xaml.zip
-Remove-Item xaml -Recurse
+Start-BitsTransfer `
+    -Source "https://globalcdn.nuget.org/packages/microsoft.ui.xaml.2.7.3.nupkg?packageVersion=2.7.3" `
+    -Destination $env:TEMP\xaml.zip
+New-Item -ItemType Directory -Path $env:TEMP\xaml
+Expand-Archive -Path $env:TEMP\xaml.zip -DestinationPath $env:TEMP\xaml
+Add-AppxPackage -Path $env:TEMP\"xaml\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
+Remove-Item $env:TEMP\xaml.zip
+Remove-Item $env:TEMP\xaml -Recurse
 
-Invoke-WebRequest `
-    -URI https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx `
-    -OutFile UWPDesktop.appx -UseBasicParsing
-Add-AppxPackage UWPDesktop.appx
-Remove-Item UWPDesktop.appx
+Start-BitsTransfer `
+    -Source "https://download.microsoft.com/download/4/7/c/47c6134b-d61f-4024-83bd-b9c9ea951c25/Microsoft.VCLibs.x64.14.00.Desktop.appx" `
+    -Destination $env:TEMP\UWPDesktop.appx
+Add-AppxPackage $env:TEMP\UWPDesktop.appx
+Remove-Item $env:TEMP\UWPDesktop.appx
 
 # Get the download URL of the latest winget installer from GitHub:
 $API_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
@@ -19,12 +19,8 @@ $DOWNLOAD_URL = $(Invoke-RestMethod $API_URL).assets.browser_download_url |
     Where-Object {$_.EndsWith(".msixbundle")}
 
 # Download the installer:
-Invoke-WebRequest -URI $DOWNLOAD_URL -OutFile winget.msixbundle -UseBasicParsing
+Start-BitsTransfer -Source $DOWNLOAD_URL -Destination $env:TEMP\winget.msixbundle
 
 # Install winget:
-Add-AppxPackage winget.msixbundle
-
-# Remove the installer:
-Remove-Item winget.msixbundle
-
+Add-AppxPackage $env:TEMP\winget.msixbundle
 echo Winget is installed.
